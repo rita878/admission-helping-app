08c9289808869d2dded3a13dd3d106ded8dfd3b3
tahminasoft <tishasultana116@gmail.com>
2025-09-04 15:50:06 +0600

    Backend server created and installed

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+// index.js
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+
+    
+
+    // Start server
+    app.listen(PORT, () => {
+      console.log(`🚀 Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err);
+  }
+}
+
+run().catch(console.dir);
